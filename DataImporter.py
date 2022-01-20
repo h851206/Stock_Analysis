bbc48e30f8ca598c6a788ca9720c2dbea8a8f74f
TingYu <h851206@gmail.com>
2022-01-20 21:38:23 +0100

    add todo list and the reference url

--- DataImporter.py
+++ DataImporter.py
@@ -1,3 +1,9 @@
+# TODO:
+# 1. Figure out some stocks (numbers) I am interested in.
+# 2. Dig into the method of making prediction
+# 3. Create a dashboard for visualization
+
+# web: https://finmind.github.io/  and  https://twstock.readthedocs.io/zh_TW/latest/prepare.html
 import pandas as pd
 import twstock
 stock = twstock.Stock('2330')
