b05775b01552f85155e97327751b943758291657
TingYu <h851206@gmail.com>
2022-01-23 16:24:10 +0100

    new StockCode.csv and finish two columns layout

--- app.py
+++ app.py
@@ -10,24 +10,37 @@ import pandas as pd
 
 
 def get_options(df):
-    Codes = df['Code'].to_list()
-    Names = df['Company'].to_list()
-    options_list = [{"label":code, "value":name} for code, name in zip(Codes, Names)]
+    Codes = df['Local Symbol'].to_list()
+    options_list = [{"label":code, "value":code} for code in Codes]
     return options_list
 
 #Load Data
-df = pd.read_csv('data/StockCode.csv', dtype={'Code': object})
+df = pd.read_csv('data/StockCode.csv', dtype={'Local Symbol': object})
 options = get_options(df)
 
 # Initialize the app
-app = dash.Dash(__name__)
-# app.config.suppress_callback_exceptions = True
-app.layout = html.Div([
-    html.Div([
-        "Multi dynamic Dropdown",
-        dcc.Dropdown(id="my-multi-dynamic-dropdown", multi=True),
-    ]),
-])
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets) 
+
+app.layout = html.Div(
+    [
+        html.Div([
+            'Please enter the stock code',
+            dcc.Dropdown(id="my-multi-dynamic-dropdown", options=[], search_value="", value=[], multi=True),
+            html.Button('Submit', id='submit-button', n_clicks=0),
+        ], className="three columns", style={"background-color": "#FFF"}),
+        html.Div([
+            html.Div(id="container-button-basic",
+                children=["No data uploaded"], 
+                className="row"
+            )
+            ]
+            , className="nine columns"),
+    ],
+)
+
+
+
 
 @app.callback(
     Output("my-multi-dynamic-dropdown", "options"),
@@ -42,6 +55,18 @@ def update_multi_options(search_value, value):
     return [
         o for o in options if search_value in o["label"] or o["value"] in (value or [])
     ]
+
+@app.callback(
+    Output("container-button-basic", "children"),
+    Input("submit-button", 'n_clicks'),
+    State("my-multi-dynamic-dropdown", "value")
+)
+def data_upload(n_clicks, value):
+    return_text = ""
+    for v in value:
+        Name = df.loc[df["Local Symbol"]==v, "Description"].squeeze()
+        return_text = return_text + " " + Name +'('+ v + ')' ",\n"
+    return return_text[:-1] + " succesfully uploaded!"
 
 
 if __name__ == "__main__":
